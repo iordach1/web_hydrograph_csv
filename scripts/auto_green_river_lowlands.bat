68aff5b41e21edf9335646432219f6134b386db4
iordach1 <42100403+iordach1@users.noreply.github.com>
2019-06-10 14:52:29 -0500

    hourly update

--- scripts/auto_green_river_lowlands.bat
+++
@@ -1,9 +0,0 @@
-cd C:\Users\iordach1\AppData\Local\Continuum\anaconda3
-
-call C:\Users\iordach1\AppData\Local\Continuum\anaconda3\Scripts\activate.bat
-
-python "G:\green_river_lowlands\scripts\gen_cc_csv.py"
-
-python "G:\green_river_lowlands\scripts\gen_hydrograph_csv.py"
-
-python "G:\green_river_lowlands\scripts\gen_mpl_hydrograph.py"
